ea527c55527f40f61d836d19e2bf9d72765fdc9d
Wacera <winniebanice@gmail.com>
2025-09-30 21:18:48 +0300

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+This ia a To-Do List application where users can add, display, and remove tasks.
+
+This task focuses on utilizing advanced DOM manipulation techniques to interact dynamically with the webpage.
+
+
+
---
+++ script.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    const addButton = document.getElementById("add-task-btn");
+    const taskInput = document.getElementById("task-input");
+    const taskList = document.getElementById("task-list");
+
+    function addTask(){
+        const taskText = taskInput.value.trim();
+        if (taskText === " "){
+            alert("Please enter a Task");
+            return;
+        }
+
+        const li = document.createElement("li");
+        li.textContent = taskText;
+
+    
+            const removeBtn = document.createElement("button");
+            removeBtn.textContent = "Remove";
+            removeBtn.className = "remove-btn";
+
+            removeBtn.onclick = function(){
+                taskList.removeChild(li);
+            };
+
+
+            li.appendChild(removeBtn);
+            taskList.appendChild(li);
+
+            taskInput.value = "";            
+        }
+    
+
+addButton.addEventListener("click", addTask);
+taskInput.addEventListener("keypress", function(event){
+    if(event.key === "Enter"){
+        addTask();
+    }
+});
+
+
+});
